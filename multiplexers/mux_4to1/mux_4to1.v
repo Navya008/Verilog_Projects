28f15e5d6039cf26380688d877dbc2697f618f76
Navya008 <navyapradeep686@gmail.com>
2025-08-04 21:29:55 +0530

    Update mux_4to1.v

--- multiplexers/mux_4to1/mux_4to1.v
+++ multiplexers/mux_4to1/mux_4to1.v
@@ -1,3 +1,3 @@
-module mux_2to1(input s,I0,I1, output y);
- assign y= (~s&I0) | (s&I1) ;
+module mux_4to1(input [1:0]s,input [3:0]I, output y);
+  assign y= (s==2'b00) ? I[0]: (s==2'b01) ? I[1]: (s==2'b10) ? I[2]: I[3] ;
 endmodule
