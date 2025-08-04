e6a9a2cfbb6cbf1825a8d0acb99e2957ad99a4a3
Navya008 <navyapradeep686@gmail.com>
2025-08-04 23:48:11 +0530

    Update tb_demux_1to2.v

--- demultiplexers/demux_1to2/tb_demux_1to2.v
+++ demultiplexers/demux_1to2/tb_demux_1to2.v
@@ -3,7 +3,7 @@ module tb_demux_1to2;
   wire [1:0]y;
   demux_1to2 uut(d,s,y);
 initial begin
-  $monitor( "s=%b d=%b =>  y0=%b y1=%b",s,d,y[0],y[1]);
+  $monitor( "s=%b d=%b =>  y=%b",s,d,y);
   d=0;
   s=1'b0; #10;
   s=1'b1; #10;
