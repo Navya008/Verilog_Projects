71cb668cb9f315c3cc052d03b9c2ca5ae8aa2192
Navya008 <navyapradeep686@gmail.com>
2025-08-05 12:01:57 +0530

    Update decoder_2to4.v

--- decoders_encoders/decoder_2to4/decoder_2to4.v
+++ decoders_encoders/decoder_2to4/decoder_2to4.v
@@ -1,4 +1,4 @@
-module decoder_2to4(input [1:0]d,output reg[3:0]o);
+module decoder_2to4(input [1:0]d,output reg[3:0]o); // Note: This design does NOT include an 'enable' input
   always @(*) begin
 o=4'b0000;
     
