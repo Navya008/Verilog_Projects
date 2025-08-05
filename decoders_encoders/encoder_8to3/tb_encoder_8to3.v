3c548c04baafa37f1505f22bb3b38fd2a85d0c77
Navya008 <navyapradeep686@gmail.com>
2025-08-05 12:27:07 +0530

    Create tb_encoder_8to3.v

---
+++ decoders_encoders/encoder_8to3/tb_encoder_8to3.v
@@ -0,0 +1,17 @@
+module tb_encoder_8to3;
+  reg [7:0]d;
+  wire [2:0]o;
+  encoder_8to3 uut(d,o);
+initial begin
+  $monitor("d=%b  => o=%b",d,o);
+  d=8'b000000001; #10;
+  d=8'b00000010; #10;
+  d=8'b00000100; #10;
+  d=8'b00001000; #10;
+  d=8'b00010000; #10;
+  d=8'b00100000; #10;
+  d=8'b01000000; #10;
+  d=8'b10000000; #10;
+  $finish;
+end
+endmodule
